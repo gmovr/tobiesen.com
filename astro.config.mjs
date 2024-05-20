da77708dd6b8581e70d4d01c46c192af69b406d1
Even Tobiesen <52675688+gmovr@users.noreply.github.com>
2024-05-20 14:32:29 +0200

    Adding Preact for some interactivity later on

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import netlify from "@astrojs/netlify";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), preact()],
   output: "server",
   adapter: netlify()
 });
