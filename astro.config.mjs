094f08b636c80af189a05b09ffbd46d226c6ca81
Even Tobiesen <52675688+gmovr@users.noreply.github.com>
2024-05-22 20:14:55 +0200

    Adding prettier, with plugins for tailwind and astro + husky for testing pre commit hooks (#8)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import netlify from "@astrojs/netlify";
@@ -9,5 +9,5 @@ import preact from "@astrojs/preact";
 export default defineConfig({
   integrations: [tailwind(), mdx(), preact()],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-        darkMode: ['selector', '[data-mode="dark"]'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-                require('@tailwindcss/typography'),
-        ],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: ["selector", '[data-mode="dark"]'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
