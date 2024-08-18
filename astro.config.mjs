af36de4649dc0b058c6d7cbc44fa6e8ea9f97c0a
Even Tobiesen <52675688+gmovr@users.noreply.github.com>
2024-08-18 16:32:43 +0200

    Adding sitemaps (#9)
    * Adding sitemaps
    
    * Update to static rendering for performance and easier sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import netlify from "@astrojs/netlify";
-
 import preact from "@astrojs/preact";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), preact()],
-  output: "server",
+  integrations: [tailwind(), mdx(), preact(), sitemap()],
+  output: "static",
+  site: "https://tobiesen.com",
   adapter: netlify(),
 });
